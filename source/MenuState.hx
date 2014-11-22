da875219f9bcc437ae2742f8eea704e9c0525f8e
Martin Delille <martin@phonations.com>
2014-11-22 09:51:49 +0100

    fix execution

--- source/MenuState.hx
+++ source/MenuState.hx
@@ -23,58 +23,58 @@ class MenuState extends FlxState
 	private var start : FlxButton;
 	private var lvlButtonGroup : FlxTypedGroup<FlxButton>;
 
-	
+
 	/**
-	 * Function that is called up when to state is created to set it up. 
+	 * Function that is called up when to state is created to set it up.
 	 */
 	override public function create():Void
 	{
-		super.create();		
-		
+		super.create();
+
 		#if mobile
 		Registre.mobileZoom = 2;
 		#else
 		Registre.mobileZoom = 1;
 		#end
-		
+
 		FlxG.debugger.visible = true;
-		
+
 		Registre.lockedLevels = [false,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true];
-		
+
 		FlxG.camera.antialiasing = true;
-		
+
 		Registre.CoefScale = new FlxPoint(FlxG.width / 1920, FlxG.height / 1080);
-		
+
 		spriteBG = new FlxSprite(0, 0);
-		spriteBG.loadGraphic('assets/images/BG.png');
+//		spriteBG.loadGraphic('assets/images/BG.png');
 		spriteBG.scale.x = Registre.CoefScale.x;
 		spriteBG.scale.y = Registre.CoefScale.y;
 		spriteBG.updateHitbox();
-		
+
 		spriteLogo = new FlxSprite(0, 0);
-		spriteLogo.loadGraphic('assets/images/logo.png');
+//		spriteLogo.loadGraphic('assets/images/logo.png');
 		spriteLogo.scale.y = spriteLogo.scale.x = Registre.CoefScale.x;
 		spriteLogo.x = 85 * Registre.CoefScale.x;
 		spriteLogo.y = 110 * Registre.CoefScale.y;
 		spriteLogo.alpha = .6;
 		spriteLogo.updateHitbox();
-		
+
 		start = new FlxButton(120 * Registre.CoefScale.x, 835 * Registre.CoefScale.x,null,gotoPlay);
 		start.width = 480 * Registre.CoefScale.x*Registre.mobileZoom;
 		start.height = 60 * Registre.CoefScale.x*Registre.mobileZoom;
 		start.alpha = 0;
 		start.kill();
-		
+
 		startText = new FlxText(120 * Registre.CoefScale.x, 835 * Registre.CoefScale.x, 480 * Registre.CoefScale.x,"START"); // x, y, width
-		startText.setFormat("assets/data/Lato-Lig.ttf", Std.int(60 * Registre.CoefScale.x), 0x000000, "center");
+//		startText.setFormat("assets/data/Lato-Lig.ttf", Std.int(60 * Registre.CoefScale.x), 0x000000, "center");
 		startText.alpha = .6;
 		startText.kill();
-		
+
 		add(spriteBG);
 		add(spriteLogo);
 		add(start);
 		add(startText);
-		
+
 		lvlButtonGroup = new FlxTypedGroup();
 		for (i in 0...24)
 		{
@@ -82,46 +82,46 @@ class MenuState extends FlxState
 			bt.loadGraphic('assets/images/button.png');
 			bt.scale.x = bt.scale.y = Registre.CoefScale.x;
 			bt.updateHitbox();
-			
+
 			if (Registre.lockedLevels[i]) bt.alpha = .3;
 			else bt.alpha = .6;
-			
+
 			lvlButtonGroup.add(bt);
-		}	
-		
+		}
+
 		add(lvlButtonGroup);
 		Registre.level = 1;
 	}
-	
+
 
 	override public function destroy():Void
 	{
 		spriteBG.destroy();
 		spriteBG = null;
-		
+
 		spriteLogo.destroy();
 		spriteLogo = null;
-		
+
 		start.destroy();
 		start = null;
-		
-		startText.destroy();	
+
+		startText.destroy();
 		startText = null;
-		
+
 		for (i in lvlButtonGroup)
 		{
 			i.destroy();
 			i = null;
 		}
-		
+
 		super.destroy();
 	}
-	
-	
+
+
 	private function onBtClick(i:Int):Void
 	{
 		Registre.level = i + 1;
-		
+
 		for (j in 0...lvlButtonGroup.members.length)
 		{
 			if (j == i) lvlButtonGroup.members[j].loadGraphic('assets/images/button02.png');
@@ -133,7 +133,7 @@ class MenuState extends FlxState
 			startText.revive();
 			startText.text = "START";
 		}
-		
+
 		else
 		{
 			start.kill();
@@ -141,7 +141,7 @@ class MenuState extends FlxState
 			startText.text = "LOCKED";
 		}
 	}
-	
+
 	private function gotoPlay():Void
 	{
 		FlxG.switchState(new PlayState());
