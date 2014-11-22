834443e05c2beff7fad31595b26f67d5913800c6
numero15 <nathanaeltardif@gmail.com>
2014-11-22 11:59:43 +0100

    merge

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -21,17 +21,12 @@ import flixel.util.FlxColor;
 import flixel.ui.FlxButton;
 import flixel.util.FlxTimer;
 import flixel.util.FlxDestroyUtil;
+import flixel.util.FlxAngle;
 import Spaceship;
 import flixel.util.FlxAngle;
 
 class PlayState extends FlxState
 {
-	private var mousePrev:FlxPoint;
-	private var mouseInitWorld:FlxPoint;
-	private var mousePrevWorld:FlxPoint;
-	private var mouseCurrentWorld:FlxPoint;
-	private var distPrev:Float;
-	private var centerPrev : FlxPoint;
 	private var timerTap : Timer;
 	private var tap : Bool;
 	private var pinch : Bool;
@@ -41,7 +36,6 @@ class PlayState extends FlxState
 	private var openPersistantBtn:FlxButton;
 //	public var cameraUI : FlxCamera;
 	public var cameraGame : FlxCamera;
-	private var mouseOrientationX :Int;
 	private var spriteBG : FlxSprite;
 	private var spriteBG_stars : FlxSprite;
 	private var player : Spaceship;
@@ -100,21 +94,13 @@ class PlayState extends FlxState
 //		openPersistantBtn.cameras = [cameraUI];
 
 		// Setup mouse and touch
-
-		mousePrev = new FlxPoint(0, 0);
-		mouseInitWorld = new FlxPoint(0, 0);
-		mouseCurrentWorld = new FlxPoint(0, 0);
-		mousePrevWorld = new FlxPoint(0, 0);
-		centerPrev = new FlxPoint(0, 0);
-		distPrev = 0;
-
 		timerTap = new Timer(1000, 0);
 
 		tap = false;
 		pinch = false;
 		scroll = false;
 		drag = false;
-		mouseOrientationX = 0;
+
 		// Setup environment
 		FlxG.debugger.visible = true;
 
@@ -164,152 +150,8 @@ class PlayState extends FlxState
 
 	override public function update():Void
 	{
-//		if (!openPersistantBtn.alive && !persistantSubState.exists)openPersistantBtn.revive();
-		#if mobile
-		for (touch in FlxG.touches.list)
-		{
-			if (touch.justPressed && FlxG.touches.list.length ==1)
-			{
+		player.angle = FlxAngle.angleBetweenMouse(player, true);
 
-				timerTap.start();
-				tap = false;
-				scroll = true;
-				drag = false;
-
-				mousePrev.x = touch.screenX;
-				mousePrev.y = touch.screenY;
-
-				mouseInitWorld.x = touch.getWorldPosition(cameraGame).x;
-				mouseInitWorld.y = touch.getWorldPosition(cameraGame).y;
-
-				if(Std.int(mouseInitWorld.x - touch.getWorldPosition(cameraGame).x)!=0)mouseOrientationX = Std.int(mouseInitWorld.x - touch.getWorldPosition(cameraGame).x);
-			}
-
-			if (touch.pressed && FlxG.touches.list.length ==1)
-			{
-				if (mousePrev.distanceTo(touch.getScreenPosition())!=0)
-				{
-					tap = false;
-				}
-
-				if (!pinch && scroll)
-				{
-					cameraGame.scroll.x -= (touch.screenX - mousePrev.x) * (1 / cameraGame.zoom);
-					cameraGame.scroll.y -= (touch.screenY - mousePrev.y) * (1 / cameraGame.zoom);
-					mousePrev.x = touch.screenX;
-					mousePrev.y = touch.screenY;
-				}
-
-				if (Std.int(mousePrevWorld.x - touch.getWorldPosition(cameraGame).x) != 0) mouseOrientationX = Std.int(mousePrevWorld.x - touch.getWorldPosition(cameraGame).x);
-				mousePrevWorld.x = touch.getWorldPosition(cameraGame).x;
-				mousePrevWorld.y = touch.getWorldPosition(cameraGame).y;
-			}
-
-			if (touch.justReleased)
-			{
-				if (timerTap.currentCount<1 && tap)
-				{
-
-				}
-				timerTap.stop();
-				timerTap.reset();
-
-				tap = false;
-				scroll = false;
-				drag = false;
-			}
-		}
-
-		if (FlxG.touches.list.length ==2)
-		{
-			tap = false;
-			scroll = false;
-			drag = false;
-			pinch = true;
-			if (distPrev != 0)
-			{
-				CameraMove.zoom((FlxG.touches.getByID(0).getWorldPosition(cameraGame).distanceTo(FlxG.touches.getByID(1).getWorldPosition(cameraGame))-distPrev)/500, Registre.LEVEL_SIZE);
-				cameraGame.scroll.x -= -centerPrev.x+(FlxG.touches.getByID(0).getWorldPosition(cameraGame).x+FlxG.touches.getByID(1).getWorldPosition(cameraGame).x)/2;
-				distPrev = FlxG.touches.getByID(0).getWorldPosition(cameraGame).distanceTo(FlxG.touches.getByID(1).getWorldPosition(cameraGame));
-			}
-
-			else
-			{
-				distPrev = FlxG.touches.getByID(0).getWorldPosition(cameraGame).distanceTo(FlxG.touches.getByID(1).getWorldPosition(cameraGame));
-				centerPrev.x =(FlxG.touches.getByID(0).getWorldPosition(cameraGame).x+FlxG.touches.getByID(1).getWorldPosition(cameraGame).x)/2;
-				centerPrev.y =(FlxG.touches.getByID(0).getWorldPosition(cameraGame).y+FlxG.touches.getByID(1).getWorldPosition(cameraGame).y)/2;
-			}
-
-		}
-		if (FlxG.touches.list.length != 2)
-		{
-			distPrev = 0;
-		}
-
-		if (FlxG.touches.list.length == 0)
-		{
-			tap = false;
-			pinch = false;
-			scroll = false;
-			drag = false;
-		}
-		#end
-
-
-		#if !mobile
-			if (FlxG.mouse.justPressed) // just pressed
-			{
-				timerTap.start();
-				tap = false;
-				scroll = true;
-
-				mouseInitWorld.x = FlxG.mouse.getWorldPosition(cameraGame).x;
-				mouseInitWorld.y = FlxG.mouse.getWorldPosition(cameraGame).y;
-			}
-
-			if (FlxG.mouse.pressed) // presse
-			{
-				if (mousePrev.distanceTo(FlxG.mouse.getScreenPosition()) !=0) // is the mouse moving ?
-				{
-					tap = false;
-
-					//scroll level
-					if (scroll)
-					{
-						cameraGame.scroll.x -= (FlxG.mouse.screenX - mousePrev.x)*(1/cameraGame.zoom);
-						cameraGame.scroll.y -= (FlxG.mouse.screenY - mousePrev.y)*(1/cameraGame.zoom);
-					}
-				}
-			}
-
-			if (FlxG.mouse.wheel != 0)
-			{
-				CameraMove.zoom(FlxG.mouse.wheel / 10);
-				CameraMove.center(mousePrevWorld, FlxG.mouse.getWorldPosition(cameraGame));
-				tap = false;
-				scroll = false;
-				drag = false;
-			}
-
-			if (FlxG.mouse.justReleased)
-			{
-				if (timerTap.currentCount<1 && tap)
-				{
-
-				}
-				timerTap.stop();
-				timerTap.reset();
-				tap = false;
-				scroll = false;
-				drag = false;
-			}
-			if(Std.int(mousePrevWorld.x - FlxG.mouse.getWorldPosition(cameraGame).x)!=0)mouseOrientationX = Std.int(mousePrevWorld.x - FlxG.mouse.getWorldPosition(cameraGame).x);
-			mousePrev.x = FlxG.mouse.screenX;
-			mousePrev.y = FlxG.mouse.screenY;
-			mousePrevWorld.x = FlxG.mouse.getWorldPosition(cameraGame).x;
-			mousePrevWorld.y = FlxG.mouse.getWorldPosition(cameraGame).y;
-
-		#end
 		super.update();
 	}
 
