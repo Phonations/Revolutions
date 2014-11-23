3d83b33d32bd8b2b80718e3fcb19098736beabd7
Martin Delille <martin@phonations.com>
2014-11-23 14:30:41 +0100

    fix mass

--- source/Planet.hx
+++ source/Planet.hx
@@ -13,12 +13,13 @@ import nape.shape.*;
 class Planet extends FlxNapeSprite
 {
 	
-	public function new(X:Float, Y:Float, type : String, mass : Int, space:Space)
+	public function new(X:Float, Y:Float, type : String, mass : Float, space:Space)
 	{
 		super(X, Y, null, false, true);
 		loadGraphic("assets/images/PlaneteStart.png");
 		//makeGraphic(512, 512, FlxColor.CORAL);
 		createCircularBody(128, BodyType.STATIC);
+		setBodyMaterial(1, 0.2, 0.4, mass);
 		body.space = space;
 	}		
 }
