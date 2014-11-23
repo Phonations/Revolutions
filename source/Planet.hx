105cfa291b1eb2cd13b1f210de60862e626ea6de
numero15 <nathanaeltardi@gmail.com>
2014-11-23 14:49:49 +0100

    add win state

--- source/Planet.hx
+++ source/Planet.hx
@@ -16,7 +16,7 @@ class Planet extends FlxNapeSprite
 	public function new(X:Float, Y:Float, type : String, mass : Float, space:Space)
 	{
 		super(X, Y, null, false, true);
-		loadGraphic("assets/images/PlaneteStart.png");
+		loadGraphic("assets/images/"+type+".png");
 		//makeGraphic(512, 512, FlxColor.CORAL);
 		createCircularBody(128, BodyType.STATIC);
 		setBodyMaterial(1, 0.2, 0.4, mass);
