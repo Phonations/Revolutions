d50cfd4006a963a1e87706ced8b6909e46b5e4fd
Martin Delille <martin@phonations.com>
2014-11-23 16:13:23 +0100

    Revert "fix radius"
    This reverts commit 8db2e318e2c5f2f2b4f5a46877a7f4fccf805a2f.
    
    Conflicts:
    	source/PlayState.hx

--- source/Planet.hx
+++ source/Planet.hx
@@ -13,20 +13,14 @@ import nape.shape.*;
 class Planet extends FlxNapeSprite
 {
 	public var type:String;
-	public function new(X:Float, Y:Float, radius:Float, _type : String, mass : Float, space:Space)
+	public function new(X:Float, Y:Float, _type : String, mass : Float, space:Space)
 	{
 		super(X, Y, null, false, true);
 		type = _type;
-
-		var size:Int = Math.ceil(radius);
-		loadGraphic("assets/images/" + type+".png");
-		FlxG.log.add(radius);
-		scale.x = radius / width;
-		scale.y = radius / height;
+		loadGraphic("assets/images/"+type+".png");
 		//makeGraphic(512, 512, FlxColor.CORAL);
-		createCircularBody(radius, BodyType.STATIC);
+		createCircularBody(128, BodyType.STATIC);
 		setBodyMaterial(1, 0.2, 0.4, mass);
 		body.space = space;
-		updateHitbox();
 	}		
 }
